50ddce4746701e56c5aed63eea744b5df7817d22
Guilherme Morgado <56857@novasbe.pt>
2026-05-13 12:19:05 +0100

    Fix sys.path insertion for module resolution

--- api/main.py
+++ api/main.py
@@ -17,6 +17,7 @@ import json
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from datetime import date, timedelta
 from functools import lru_cache
